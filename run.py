bfa7c44f952eeccc9b7b46ff74e5c487ddde3c96
thienan2050 <thienan2050@gmail.com>
2021-08-03 11:11:19 +0000

    Allow user to add path

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
+path = '/home/pi/RPi/Project-ID-31028294'
+
 media = []
 temptFile = ''
 temptFile = ''
-for t in os.listdir(os.getcwd()):
+for t in os.listdir(path):
     if t.endswith(".txt"):
-        temptFile = os.path.join(os.getcwd(), t)
+        temptFile = os.path.join(path, t)
         with open(temptFile, 'r') as r:
             index = int(r.read())
             print("Play video " + str(index))
@@ -14,15 +16,15 @@ if temptFile == '':
     print("Play video " + str(index))
 
     
-for file in os.listdir(os.getcwd()):
+for file in os.listdir(path):
     if file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".mpeg") :
-        media.append(os.path.join(os.getcwd(), file))
+        media.append(os.path.join(path, file))
 
 
 print("omxplayer -o hdmi "+media[index])
 os.system("omxplayer -o hdmi "+media[index])
 index = (index + 1) % len(media)
-with open("tempt.txt",'w') as f:
+with open(path+"/tempt.txt",'w') as f:
     f.write(str(index))
 #os.system("omxplayer -o hdmi "+media[index])
 
