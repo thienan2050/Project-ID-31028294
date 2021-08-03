c3886cdf5de60a79e2530c25a2ac55c01def7224
thienan2050 <thienan2050@gmail.com>
2021-08-03 15:32:50 +0000

    Fix ValueError: invalid literal for int() with base 10

--- run.py
+++ run.py
@@ -4,29 +4,30 @@ path = '/home/pi/RPi/Project-ID-31028294'
 media = []
 temptFile = ''
 temptFile = ''
-for t in os.listdir(path):
-    if t.endswith(".txt"):
-        temptFile = os.path.join(path, t)
-        with open(temptFile, 'r') as r:
-            index = int(r.read())
-            print("Play video " + str(index))
-            
-if temptFile == '':        
-    index = 0
-    print("Play video " + str(index))
+index = 0
 
-    
 for file in os.listdir(path):
     if file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".mpeg") :
         media.append(os.path.join(path, file))
 
-
-print("omxplayer -o hdmi "+media[index])
-os.system("omxplayer -o hdmi "+media[index])
+for root, dirs, files in os.walk(path):
+    if 'tempt.txt' in files:
+        f = open(path + '/tempt.txt')
+        try:
+            index = int(f.read())
+            f.close()
+        except:
+            index = 0
+            
+with open(path + "/tempt.txt",'w') as f:
+    f.write(str(index)) 
+print('Play video '+ str(index))
+print("omxplayer -o hdmi " + media[index])
+os.system("omxplayer -o hdmi " + media[index])
 index = (index + 1) % len(media)
-with open(path+"/tempt.txt",'w') as f:
+with open(path + "/tempt.txt",'w') as f:
     f.write(str(index))
-#os.system("omxplayer -o hdmi "+media[index])
+
 
 
  
